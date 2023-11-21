5e430757cfe6f53c3908403c71d52e1ef2323186
Cora Zhu <zhiyi.zhu@lincolnuni.ac.nz>
2023-11-21 21:39:42 +0000

    example pie chart

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
+import plotly.graph_objects as go
 
 app = Dash(__name__)
 
-app.layout = html.Div([
-    html.H1(children='Hello World', style={'textAlign': 'center'})
-])
+
+labels = ['Oxygen','Hydrogen','Carbon_Dioxide','Nitrogen']
+values = [4500, 2500, 1053, 500]
+
+# Use `hole` to create a donut-like pie chart
+fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3)])
+fig.show()
 
 if __name__ == '__main__':
     app.run(debug=True)
