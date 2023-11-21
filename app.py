76caa65e1fffab372019c4853d5b8de4e85ce63a
Cora Zhu <zhiyi.zhu@lincolnuni.ac.nz>
2023-11-21 21:47:57 +0000

    add a display section

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from dash import Dash, html, dcc, callback, Output, Input
-import plotly.express as px
-import pandas as pd
 import plotly.graph_objects as go
 
 app = Dash(__name__)
 
-
-labels = ['Oxygen','Hydrogen','Carbon_Dioxide','Nitrogen']
+labels = ['Oxygen', 'Hydrogen', 'Carbon_Dioxide', 'Nitrogen']
 values = [4500, 2500, 1053, 500]
 
-# Use `hole` to create a donut-like pie chart
+
 fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3)])
-fig.show()
+
+app.layout = html.Div([
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run_server(debug=True)
