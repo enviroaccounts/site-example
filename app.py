cdef7b4c498db1932907b860588d0daac3fd0b76
Cora Zhu <zhiyi.zhu@lincolnuni.ac.nz>
2023-11-23 00:09:07 +0000

    layout updated

--- app.py
+++ app.py
@@ -35,18 +35,23 @@ pie_chart_data = get_piechart_data()
 fig_pie_chart = go.Figure(data=[go.Pie(labels=pie_chart_data['year'], values=pie_chart_data['percent'], hole=.3, name='Year', marker=dict(colors=colors))])
 
 initial_barchart_data = get_barchart_data(pie_chart_data['year'].iloc[0])
-fig_bar_chart = px.bar(initial_barchart_data, x='Type', y='percent', color_discrete_sequence=colors)
-
-# Enable transitions
-fig_bar_chart.update_layout(transition_duration=500)
+fig_bar_chart = px.bar(initial_barchart_data, x='Type', y='percent')
 
 # Layout
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-    dcc.Graph(id='pie-chart', figure=fig_pie_chart),
-    html.Div(children='Dash: A web application framework for your data.'),
-    dcc.Graph(id='bar-chart', figure=fig_bar_chart)
-])
+    html.Div([  
+    html.Div([
+        html.H1(children='Data Dashboard'),
+        html.H2(children='Project:'),
+        html.H4(children='Interactive charts for frontend data visualization using dash and plotly')
+    ],className="description"), 
+    html.Div([
+        dcc.Graph(id='pie-chart', figure=fig_pie_chart)
+    ],className="pie_chart"),
+    html.Div([ 
+        dcc.Graph(id='bar-chart', figure=fig_bar_chart)
+            ],className="bar_chart")],className="visualisation container")
+        ])
 
 # Callback for interactivity
 @app.callback(
@@ -56,7 +61,12 @@ app.layout = html.Div(children=[
 def update_bar_chart(clickData):
     selected_year = clickData['points'][0]['label'] if clickData else pie_chart_data['year'].iloc[0]
     new_barchart_data = get_barchart_data(selected_year)
-    return px.bar(new_barchart_data, x='Type', y='percent', color_discrete_sequence=colors).update_layout(transition_duration=500)
+
+    # Determine the color based on the selected year
+    color_index = pie_chart_data['year'][pie_chart_data['year'] == int(selected_year)].index[0]
+    selected_color = [colors[color_index]] * len(new_barchart_data)
+
+    return px.bar(new_barchart_data, x='Type', y='percent', color_discrete_sequence=selected_color).update_layout(transition_duration=500)
 
 if __name__ == '__main__':
     app.run_server(debug=True, host='0.0.0.0', port=8050)
