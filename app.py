2e0039da8242a1580f2dde452d77e37ac645ac76
Cora Zhu <zhiyi.zhu@lincolnuni.ac.nz>
2023-11-27 06:31:16 +0000

    LandUseChange_Forest chart completed

--- app.py
+++ app.py
@@ -1,74 +1,49 @@
-from dash import Dash, html, dcc, callback, Output, Input
+from dash import Dash, html, dcc
 import pandas as pd
 import plotly.graph_objects as go
-import numpy as np
-import plotly.express as px
 
-app = Dash(__name__)
+def load_forest_land_use_data():
+    """Loads forest land use change data."""
+    return pd.read_csv("static/data/LandUseChange_Forest_1990_2016.csv")
 
+def prepare_forest_land_use_chart_data(data_df):
+    """Prepares data for the forest land use pie chart."""
+    land_use_data = data_df.iloc[0, 3:]  # land use columns start from the 4th column
+    labels = land_use_data.index.tolist()
+    values = land_use_data.values.tolist()
+    return labels, values
 
-# Reading data
-data_df = pd.read_csv("static/data/Pests.csv")
+def create_forest_land_use_pie_chart(labels, values):
+    """Creates a pie chart for forest land use data."""
+    return go.Figure(data=[go.Pie(labels=labels, values=values)])
 
-# Function to calculate the percentage
-def calculate_percentage(val, total):
-    percent = np.round((np.divide(val, total) * 100), 2)
-    return percent
-
-# Function to get pie chart data
-def get_piechart_data():
-    year_data = data_df.groupby('year')['count'].sum().reset_index()
-    year_data['percent'] = calculate_percentage(year_data['count'], year_data['count'].sum())
-    return year_data
-
-# Function to get bar chart data for a specific year
-def get_barchart_data(selected_year):
-    year_data = data_df[data_df['year'] == int(selected_year)]
-    year_grouped = year_data.groupby('Type', observed=False)['count'].sum().reset_index()
-    year_grouped['percent'] = calculate_percentage(year_grouped['count'], year_grouped['count'].sum())
-    return year_grouped
-
-# Color palette for charts
-colors = px.colors.qualitative.Plotly
-
-# Initial figures
-pie_chart_data = get_piechart_data()
-fig_pie_chart = go.Figure(data=[go.Pie(labels=pie_chart_data['year'], values=pie_chart_data['percent'], hole=.3, name='Year', marker=dict(colors=colors))])
-
-initial_barchart_data = get_barchart_data(pie_chart_data['year'].iloc[0])
-fig_bar_chart = px.bar(initial_barchart_data, x='Type', y='percent')
-
-# Layout
-app.layout = html.Div(children=[
-    html.Div([  
-    html.Div([
-        html.H1(children='Data Dashboard - revised'),
-        html.H2(children='Project:'),
-        html.H4(children='Interactive charts for frontend data visualization using dash and plotly')
-    ],className="description"), 
-    html.Div([
-        dcc.Graph(id='pie-chart', figure=fig_pie_chart)
-    ],className="pie_chart"),
-    html.Div([ 
-        dcc.Graph(id='bar-chart', figure=fig_bar_chart)
-            ],className="bar_chart")],className="visualisation container")
+def setup_dash_layout(app, fig_pie_chart):
+    """Sets up the layout of the Dash app."""
+    app.layout = html.Div(children=[
+        html.Div([
+            dcc.Graph(id='forest-land-use-pie-chart', figure=fig_pie_chart)
+        ]),
+        html.Div([  
+            html.H3(id='forest-land-use-pie-chart-description',children='Land uses converted from forestland since 1990.')
         ])
+    ],id='forest-land-use-pie-chart-layout')
 
-# Callback for interactivity
-@app.callback(
-    Output('bar-chart', 'figure'),
-    [Input('pie-chart', 'clickData')]
-)
-def update_bar_chart(clickData):
-    selected_year = clickData['points'][0]['label'] if clickData else pie_chart_data['year'].iloc[0]
-    new_barchart_data = get_barchart_data(selected_year)
+def create_app():
+    """Creates and configures the Dash app."""
+    app = Dash(__name__)
 
-    # Determine the color based on the selected year
-    color_index = pie_chart_data['year'][pie_chart_data['year'] == int(selected_year)].index[0]
-    selected_color = [colors[color_index]] * len(new_barchart_data)
+    # Load and prepare data
+    data_df = load_forest_land_use_data()
+    labels, values = prepare_forest_land_use_chart_data(data_df)
 
-    return px.bar(new_barchart_data, x='Type', y='percent', color_discrete_sequence=selected_color).update_layout(transition_duration=500)
+    # Create pie chart
+    fig_pie_chart = create_forest_land_use_pie_chart(labels, values)
 
+    # Setup layout
+    setup_dash_layout(app, fig_pie_chart)
+
+    return app
 
 if __name__ == '__main__':
+    app = create_app()
     app.run_server(debug=True, host='0.0.0.0', port=8050)
