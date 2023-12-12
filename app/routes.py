84b7a9cf661414727eb72b8a742cc543012886c5
Cora Zhu <zhiyi.zhu@lincolnuni.ac.nz>
2023-12-12 16:52:05 +1300

    refac

--- app/routes.py
+++ app/routes.py
@@ -9,4 +9,14 @@ app.jinja_env.globals.update(create_figure=create_figure)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('pages/index.html')
+
+
+@app.route('/main')
+def main():
+    return render_template('pages/main.html')
+
+
+@app.route('/water')
+def water():
+    return render_template('pages/water/index.html')
