900fe4cfb6059f4c07a5b75aa536d385a343a98f
Cora Zhu <zhiyi.zhu@lincolnuni.ac.nz>
2024-01-09 17:25:21 +1300

    routes updated

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 """Routes for parent Flask app."""
 from flask import current_app as app
-from flask import render_template
+from flask import render_template,redirect
 from .plots import LandUses
 from .plots import LandUsesCopy
 
@@ -10,16 +10,39 @@ app.jinja_env.globals.update(LandUsesCopy=LandUsesCopy)
 
 
 @app.route('/')
+def home():
+    return redirect("/landuse_change")
+
+@app.route('/landuse_change')
+def landuse_change():
+    return render_template("pages/landuse_change.html")
+
+
+@app.route('/acknowledgements')
+def acknowledgements():
+    return render_template("pages/acknowledgements.html")
+
+@app.route('/climate_variables')
+def climate_variables():
+    return render_template("pages/climate_variables.html")
+
+@app.route('/invasive_species')
+def invasive_species():
+    return render_template("pages/invasive_species.html")
+
+@app.route('/social_well_being')
+def social_well_being():
+    return render_template("pages/social_well_being.html")
+
+@app.route('/main')
+def main():
+    return render_template("pages/main.html")
+
+@app.route('/greenhouse_gas_emissions')
+def greenhouse_gas_emissions():
+    return render_template("pages/greenhouse_gas_emissions.html")
+
+@app.route('/index')
 def index():
-    return render_template('pages/index.html')
+    return render_template("pages/index.html")
 
-
-
-@app.route('/<page>')
-def main(page):
-    return render_template(f"pages/{page}.html")
-
-
-@app.route('/water')
-def water():
-    return render_template('pages/water/index.html')
